37e292d23aa25bca450a557638f8d948cca0aa36
profbunbun <aaronrls1984@gmail.com>
2023-07-08 11:07:17 -0400

    new template

---
+++ Agent/DQNetwork.py
@@ -0,0 +1,36 @@
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+
+
+
+# if GPU is to be used
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    
+    
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
